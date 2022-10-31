9f72431dba8b72f14d2c5ef488f5e3ee063d9c18
Alexander <bergersen.alexander@gmail.com>
2022-10-31 14:06:29 +0100

    just removing some testing

--- examples/python/nba_prediction/data_cleaning_awards.py
+++ examples/python/nba_prediction/data_cleaning_awards.py
@@ -7,24 +7,22 @@ import pyspark.sql.types as T
 
 spark = get_spark_session()
 
-spark.sql("describe detail MvpsWithStats").show(truncate=False)
+award_winners = read_from_csv(spark, "nba_prediction/datasets/Player Award Shares.csv")\
+    .select("season", "player_id", "share", "award")\
+    .withColumnRenamed("season", "season_award")\
+    .withColumnRenamed("player_id", "player_id_award")
 
-# award_winners = read_from_csv(spark, "nba_prediction/datasets/Player Award Shares.csv")\
-#     .select("season", "player_id", "share", "award")\
-#     .withColumnRenamed("season", "season_award")\
-#     .withColumnRenamed("player_id", "player_id_award")
+player_per_game_stats = get_player_per_game_stats(spark)
 
-# player_per_game_stats = get_player_per_game_stats(spark)
+award_conditions = [
+    player_per_game_stats.player_id == award_winners.player_id_award,
+    player_per_game_stats.season == award_winners.season_award
+]    
 
-# award_conditions = [
-#     player_per_game_stats.player_id == award_winners.player_id_award,
-#     player_per_game_stats.season == award_winners.season_award
-# ]    
+award_winners_with_stats = player_per_game_stats.join(award_winners, award_conditions, "inner").drop("player_id_award", "season_award")\
+    .withColumn("share", F.col("share").cast(T.FloatType()))
+mvps_with_stats = award_winners_with_stats.filter("award = 'nba mvp'").drop("award")
+dpoys_with_stats = award_winners_with_stats.filter("award = 'dpoy'").drop("award")
 
-# award_winners_with_stats = player_per_game_stats.join(award_winners, award_conditions, "inner").drop("player_id_award", "season_award")\
-#     .withColumn("share", F.col("share").cast(T.FloatType()))
-# mvps_with_stats = award_winners_with_stats.filter("award = 'nba mvp'").drop("award")
-# dpoys_with_stats = award_winners_with_stats.filter("award = 'dpoy'").drop("award")
-
-# save_to_delta_table(mvps_with_stats, "MvpsWithStats")
-# save_to_delta_table(dpoys_with_stats, "DpoysWithStats")
+save_to_delta_table(mvps_with_stats, "MvpsWithStats")
+save_to_delta_table(dpoys_with_stats, "DpoysWithStats")
