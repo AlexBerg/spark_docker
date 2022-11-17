1f246dcdeff4c75839cf31c1706f4cda1cb9ba45
Alexander <bergersen.alexander@gmail.com>
2022-11-17 13:37:56 +0100

    setting up minio with hive user and creating hive bucket as initialization

--- docker/shared/templates/minio-scripts/setup-minio.sh
+++
@@ -1,5 +0,0 @@
-minio server /data --console-address ":9001"
-
-mc alias set myminio http://localhost:9000 minio_user minio_password
-mc admin user add myminio minio_hive_user Supersecretpassw0rd!
-mc mb myminio/hive
