a8329677edbc2509c4e21db53c9f6684ef047cfa
Alexander <bergersen.alexander@gmail.com>
2022-11-17 15:59:22 +0100

    updating dummy passwords and trying to connect to minio as warehouse for hive

--- docker/shared/templates/minio-scripts/init-minio.sh
+++ docker/shared/templates/minio-scripts/init-minio.sh
@@ -2,7 +2,7 @@ nohup minio server /data --console-address ":9001" &
 
 sleep 5
 
-mc alias set myminio http://localhost:9000 minio_user minio_password
-mc admin user add myminio minio_hive_user Supersecretpassw0rd!
+mc alias set myminio http://localhost:9000 <ROOTADMIN> <ROOTPASSWORD>
+mc admin user add myminio minio_hive_user <HIVEUSERPASSWORD>
 mc mb myminio/hive
 mc admin policy set myminio readwrite user=minio_hive_user
