7adfda155a6e7c63781611abda696b26bab13487
Alexander <bergersen.alexander@gmail.com>
2022-11-29 09:43:36 +0100

    updated minio contaienr to used mapped volume, changed init script to be entrypoint script

--- docker/shared/templates/minio-scripts/init-minio.sh
+++ docker/shared/templates/minio-scripts/init-minio.sh
@@ -1,4 +1,4 @@
-nohup minio server /data --console-address ":9001" &
+minio server /data --console-address ":9001" &
 
 sleep 5
 
@@ -6,3 +6,11 @@ mc alias set myminio http://localhost:9000 <ROOTADMIN> <ROOTPASSWORD>
 mc admin user add myminio minio_hive_user <HIVEUSERPASSWORD>
 mc mb myminio/hive
 mc admin policy set myminio readwrite user=minio_hive_user
+
+# When doing the above in the entrypoint, the docker container exists beliving the server is no longer running (even with nohup).
+# Adding the below to shut the server down and restart it to keep the continer (and minio server) going.
+mc admin service stop myminio
+
+sleep 2
+
+minio server /data --console-address ":9001"
