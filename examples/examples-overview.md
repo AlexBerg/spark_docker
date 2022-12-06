3b4c59836753230dc5a79683facb64b1e9fe01ed
Alexander <bergersen.alexander@gmail.com>
2022-12-06 23:11:52 +0100

    updated overview for python example and updated reference to renamed method

--- examples/examples-overview.md
+++ examples/examples-overview.md
@@ -1,3 +1,30 @@
 # NBA prediction example projects
 There are two example projects, one python and one dotnet, that aim to show how this docker setup can be used for local spark development and testing.
 The example projects use dataset with [NBA stats from 1947 to present](https://www.kaggle.com/datasets/sumitrodatta/nba-aba-baa-stats?datasetId=1177523) found on [Kaggle](https://www.kaggle.com/) to train and evaluate models that try to predict the NBA mvp winner based on full season stats.
+
+If you want to use iceberg instead of delta replace the following:
+
+```
+.config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")\
+.config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
+```
+for 
+```
+.config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")\
+.config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
+```
+in helper_functions.get_spark_session
+
+and
+
+```
+df.writeTo(tableName).using("delta").create()
+```
+
+for
+
+```
+df.writeTo(tableName).using("iceberg").create()
+```
+
+in helper_functions.write_to_table
--- examples/python/nba_prediction/data_modeling.py
+++ examples/python/nba_prediction/data_modeling.py
@@ -1,4 +1,4 @@
-from helper_functions import get_spark_session, read_from_csv, write_to_delta_table
+from helper_functions import get_spark_session, read_from_csv, write_to_table
 from pyspark.sql import SparkSession, DataFrame
 
 import pyspark.sql.functions as F
@@ -70,7 +70,7 @@ def _create_team_tables(spark: SparkSession) -> DataFrame:
         .distinct()\
         .withColumn("TeamId", F.concat(F.hex(F.col("League")), F.hex(F.col("TeamNameShort"))))
     
-    write_to_delta_table(teams, "Teams")
+    write_to_table(teams, "Teams")
 
     season_stats_cond = [team_summaries.abbreviation == teams.TeamNameShort, team_summaries.lg == teams.League]
     team_season_stats = team_summaries.join(teams, season_stats_cond)\
@@ -108,7 +108,7 @@ def _create_team_tables(spark: SparkSession) -> DataFrame:
 
     team_season_stats = _cast_column_to_float(team_season_stats)
 
-    write_to_delta_table(team_season_stats, "TeamSeasonStats")
+    write_to_table(team_season_stats, "TeamSeasonStats")
 
     return return_df
 
@@ -131,7 +131,7 @@ def _create_players_table(spark: SparkSession):
             F.col("player").alias("PlayerName"),
             F.col("hof").alias("MadeHallOfFame").cast(T.BooleanType()))
 
-    write_to_delta_table(player_info, "Players")
+    write_to_table(player_info, "Players")
 
 
 def _create_player_award_share_table(spark: SparkSession, teams: DataFrame):
@@ -149,7 +149,7 @@ def _create_player_award_share_table(spark: SparkSession, teams: DataFrame):
             F.col("share").alias("Share").cast(T.FloatType()),
             F.col("winner").alias("WonAward").cast(T.BooleanType()))
 
-    write_to_delta_table(player_award_share, "PlayerSeasonAwardShare")
+    write_to_table(player_award_share, "PlayerSeasonAwardShare")
 
 
 def _create_player_season_advanced_stats_tables(spark: SparkSession, teams: DataFrame):
@@ -184,7 +184,7 @@ def _create_player_season_advanced_stats_tables(spark: SparkSession, teams: Data
 
     advanced_stats = _cast_column_to_float(advanced_stats)
 
-    write_to_delta_table(advanced_stats, "PlayerSeasonAdvancedStats")
+    write_to_table(advanced_stats, "PlayerSeasonAdvancedStats")
 
 
 def _create_player_season_stats_table(spark: SparkSession, teams: DataFrame):
@@ -225,7 +225,7 @@ def _create_player_season_stats_table(spark: SparkSession, teams: DataFrame):
 
     player_per_game_stats = _cast_column_to_float(player_per_game_stats)
 
-    write_to_delta_table(player_per_game_stats, "PlayerSeasonStats")
+    write_to_table(player_per_game_stats, "PlayerSeasonStats")
 
 
 
@@ -253,7 +253,7 @@ def _create_player_play_by_play_table(spark: SparkSession, teams: DataFrame):
 
     play_by_play = _cast_column_to_float(play_by_play)
 
-    write_to_delta_table(play_by_play, "PlayerSeasonPlayByPlayStats")
+    write_to_table(play_by_play, "PlayerSeasonPlayByPlayStats")
 
 
 def _cast_column_to_float(df: DataFrame) -> DataFrame:
